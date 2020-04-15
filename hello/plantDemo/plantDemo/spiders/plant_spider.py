c3993e501a1473665af443d670fce0a03bda669c
nada-lasri <nada.lasri0@gmail.com>
2020-04-15 15:30:21 +0200

    scraping des valeurs du site 1 ok, formatage des valeurs restant

--- hello/plantDemo/plantDemo/spiders/plant_spider.py
+++ hello/plantDemo/plantDemo/spiders/plant_spider.py
@@ -9,14 +9,41 @@ class PlantsSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-        'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=headphones&rh=i%3Aaps%2Ck%3Aheadphones&ajr=2',
+        'https://jardinage.ooreka.fr/plante/voir/16/basilic',
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        img_urls = response.css('img::attr(src)').extract()
-        with open('urls.txt', 'w') as f:
-            for u in img_urls:
-                f.write(u + "\n")
+        yield {
+            'nom': response.xpath('//*[@id="fiches_plantes"]/h1/text()').extract(),
+        }
+        yield {
+            'famille': response.xpath('//*[@id="resume_plante"]/div[1]/ul/li[3]/p[2]/text()').extract(),
+        }
+        yield {
+            'entretien': response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[1]/div/span/text()').extract()[1:2],
+        }
+        yield {
+            'besoinEau': response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[2]/div/span/text()').extract()[1:2],
+        }
+        yield {
+            'croissance': response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[3]/div/span/text()').extract()[1:2],
+        }
+        yield {
+            'resistanceFroid': response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[5]/div/span/text()').extract()[1:2],
+        }
+        yield {
+            'exposition': response.xpath('//*[@id="resume_plante"]/div[4]/ul/li[1]/div/div/text()').extract()[1:2],
+        }
+        yield {
+            'hauteurMaturite': response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/div/span/text()').extract(),
+        }
+        yield {
+            'largeurMaturite': response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[7]/div/span/text()').extract(),
+        }
+
+        #with open('urls.txt', 'w') as f:
+        #    for u in img_urls:
+        #        f.write(u + "\n")
