c34a39d76a3ad05fa9d05cbb3e07352b33472eb1
nada-lasri <nada.lasri0@gmail.com>
2020-04-15 14:08:53 +0200

    scrapping for pommier

---
+++ hello/plantDemo/plantDemo/spiders/plant_spider.py
@@ -0,0 +1,22 @@
+#type in 2:Python:
+#PS C:\PLD SMART\Vivai-Back\hello\plantDemo\plantDemo\spiders> scrapy crawl plants
+
+import scrapy
+
+class PlantsSpider(scrapy.Spider):
+
+    name = "plants"
+
+    def start_requests(self):
+        urls = [
+        'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=headphones&rh=i%3Aaps%2Ck%3Aheadphones&ajr=2',
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        img_urls = response.css('img::attr(src)').extract()
+        with open('urls.txt', 'w') as f:
+            for u in img_urls:
+                f.write(u + "\n")
