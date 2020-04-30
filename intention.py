2954b84f67019bb634cf27590f1194b485661529
Jean <jean@ababii.fr>
2020-04-30 16:06:20 +0200

    getIntention function
    function to call to get the intention and the files needed

---
+++ intention.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import spacy
+from joblib import load
+
+def getIntention(sentence):
+    
+    #load trained model
+    clf_svm = load('clf_svm.joblib')
+    
+    # Pre-processing
+    sentence = sentence.lower()
+
+    nlp_fr = spacy.load('fr_core_news_sm')
+    tokens = nlp_fr(sentence)
+
+    words = []
+    allowed_pos = ['VERB', 'NOUN', 'PROPN', 'ADJ']
+    vectorizer = load('vectorizer.joblib')
+                
+    # Lemmatize
+    for token in tokens:
+        if ( allowed_pos.count(token.pos_) > 0 ):
+            if (token.lemma_ != 'plante' and token.lemma_ != 't'): 
+                words.append(str(token.lemma_))          
+            j = 0;
+            vector = vectorizer.get_feature_names()
+
+    # Create vector
+    for word in vector:
+        vector[j] = words.count(word);           
+        j += 1
+
+
+    p = clf_svm.predict([vector])
+    
+    intentions = load('intentions.joblib')
+    return(intentions[p])
