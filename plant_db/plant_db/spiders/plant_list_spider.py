f00ad936f288bd63db26c6a8c78a7ddabf5401fc
nada-lasri <nada.lasri0@gmail.com>
2020-05-04 21:03:50 +0200

    json generate for plant-url couple database

--- plant_db/plant_db/spiders/plant_list_spider.py
+++ plant_db/plant_db/spiders/plant_list_spider.py
@@ -8,15 +8,49 @@ class PlantListSpider(scrapy.Spider):
 
     start_urls = [
         'https://jardinage.ooreka.fr/plante/rechercheAlpha/a',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/b',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/c',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/d',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/e',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/f',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/g',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/h',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/i',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/j',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/k',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/l',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/m',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/n',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/o',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/p',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/q',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/r',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/s',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/t',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/u',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/v',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/w',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/x',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/y',
+        'https://jardinage.ooreka.fr/plante/rechercheAlpha/z',
     ]
 
     def parse(self, response):
-        plantList = {}
+        
         i = 0
         for plant in response.css('div.info_plante'):
             url = plant.css('a.titre_liste_plante::attr(href)').get()
             species = plant.css('a.titre_liste_plante::text').get()
-            plantList[i] = "{ \"species\": {\"S\": " + species + "}, \"websiteUrl\": {\"S\": https://jardinage.ooreka.fr"+ url + "} }"
+            yield {
+                i : "{ \"species\": {\"S\": " + species + "}, \"websiteUrl\": {\"S\": https://jardinage.ooreka.fr"+ url + "} }"
+            }
             i = i + 1
-        
-        return plantList
+
+                # next_page = /page-{}/ where {} number of page.
+        next_page_short = response.xpath('//*[@id="plant-search-result"]/div[21]/div[3]/a/@href').extract_first()
+
+        # If next_page have value
+        if next_page_short is not None:
+            next_page =  "https://jardinage.ooreka.fr" + next_page_short
+            # Recall parse with url https://sanet.st/page-{}/ where {} number of page.
+            yield scrapy.Request(url=next_page, callback=self.parse)
