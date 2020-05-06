5638ece04328ee87935c98fea4960a9e8f8920d3
nada-lasri <nada.lasri0@gmail.com>
2020-05-06 18:07:25 +0200

    all attributes except plantationMonths,whereToPlant,pest,ecological, pest, ecologicaTips, history

--- plant_info/plant_info/spiders/plant_info_spider.py
+++ plant_info/plant_info/spiders/plant_info_spider.py
@@ -16,31 +16,90 @@ class PlantInfoSpider(scrapy.Spider):
                 yield self.make_requests_from_url(self.url)
 
     def parse(self, response):
+
+        typeItems = response.xpath('//*[@id="resume_plante"]/div[1]/ul/li[4]//p/text()').extract()
+        typeString = ""
+        for p  in typeItems[1:] :
+            p = p.replace('\t','').replace('\n','')
+            typeString = typeString + p
+
+        def data_parse(xpathResponse) :
+            items = response.xpath(xpathResponse).extract()
+            itemString = ""
+            for p  in items :
+                p = p.replace('\t','').replace('\n','')
+                if p :
+                    itemString = itemString + p
+            return itemString
+
+        def data_clean(xpathResponse) :
+            items = response.xpath(xpathResponse).extract()
+            for p in items :
+                p = re.sub('<[^>]+>', '', p)
+            return items
+
+        def height_test() :
+            default = ""
+
+            if any("Hauteur" in s for s in response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[4]/p/text()').extract()) :
+                return re.sub('<[^>]+>', '', response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[4]/div/span').extract()[0]).strip('\n').strip('\t').strip('\n')
+            elif any("Hauteur" in s for s in response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/p/text()').extract()) :
+                return re.sub('<[^>]+>', '', response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/div/span').extract()[0]).strip('\n').strip('\t').strip('\n')
+            elif any("Hauteur" in s for s in response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[5]/p/text()').extract()) :
+                return re.sub('<[^>]+>', '', response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/div/span').extract()[0]).strip('\n').strip('\t').strip('\n')
+            else :
+                return default
+
+        def width_test() :
+            default = ""
+            if any("Largeur" in s for s in response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/p').extract()) :
+                return re.sub('<[^>]+>', '', response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[6]/div/span').extract()[0]).strip('\n').strip('\t').strip('\n')
+            if any("Largeur" in s for s in response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[7]/p').extract()) :
+                return re.sub('<[^>]+>', '', response.xpath('//*[@id="resume_plante"]/div[2]/ul/li[7]/div/span').extract()[0]).strip('\n').strip('\t').strip('\n')
+
+            else :
+                return default
+
+        def outdoor_test() :
+            default = ""
+
+            if any("ext" in s for s in response.xpath('//*[@id="resume_plante"]/div[4]/ul/li[3]/p').extract()) :
+                return data_parse('//*[@id="resume_plante"]/div[4]/ul/li[3]/div/span/text()')
+            if any("ext" in s for s in response.xpath('//*[@id="resume_plante"]/div[4]/ul/li[2]/p').extract()) :
+                return data_parse('//*[@id="resume_plante"]/div[4]/ul/li[2]/div/span/text()')
+            else :
+                return default
+
+        def indoor_test() :
+            default = ""
+
+            if any("int" in s for s in response.xpath('//*[@id="resume_plante"]/div[4]/ul/li[2]/p').extract()) :
+                return data_parse('//*[@id="resume_plante"]/div[4]/ul/li[2]/div/span/text()')
+            else :
+                return default
+
         yield {
             'species' : response.xpath('//*[@id="fiches_plantes"]/h1/text()').get(),
             'picUrl' : response.xpath('//*[@id="image-0"]/a/img/@src').get(),
-            'decription' : response.xpath('//*[@id="fiches_plantes"]/div[2]/div[2]/div[1]/p[1]/text()').get(),
+            'description' : response.xpath('//*[@id="fiches_plantes"]/div[2]/div[2]/div[1]/p[1]/text()').get(),
             'latinName' : response.xpath('//*[@id="resume_plante"]/div[1]/ul/li[2]/p[2]/text()').get(),
             'family' : response.xpath('//*[@id="resume_plante"]/div[1]/ul/li[3]/p[2]/text()').get(),
-            #'type' : Arbuste à fleurs | Arbuste fruitier |Plante (.replace <p> "")
-            #'vegetation' : Vivace
-            #'height' : 1 à 5 m
-            #'width' : 1 à3 (attention try catch) (null)
+            'type' : typeString,
+            'vegetation' : data_parse('//*[@id="resume_plante"]/div[2]/ul/li[3]/div//span/text()'),
+            'height' : height_test(),
+            'width' : width_test(),
             'careLevel' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[1]/div/span/text()')[1].get()),
             'waterNeed' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[2]/div/span/text()')[1].get()),
             'growth' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[3]/div/span/text()')[0].get()),
-            'coldResitance' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[5]/div/span/text()')[1].get()),
-            #'soilType' : Sol argileux
+            'coldResistance' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[3]/ul/li[5]/div/span/text()')[1].get()),
+            'soilType' : data_parse('//*[@id="resume_plante"]/div[3]/ul/li[6]/div//span/text()'),
             'sunNeed' : re.sub('\s+', '', response.xpath('//*[@id="resume_plante"]/div[4]/ul/li[1]/div/div/text()')[1].get()),
-            #'indoorUse' : Véranda
-            #'outdoorUse' : Balcon ou terrasse
+            'indoorUse' : indoor_test(),
+            'outdoorUse' : outdoor_test(),
             #'plantationMonths' : Avril, Mai, Juin, Julliet, Aout (list)
-            #'whereToPlant' : Plantez-le dans un gros 
-            'pest' : response.xpath('//*[@id="fiches_plantes"]/div[2]/div[2]/div[10]/div[8]/div/p').getall()[1],
-            #'ecologicalTips' : (null)
-            #'history' : (null) tableau avec un paragraphe par case
-        
-        
-        
+            #'whereToPlant' : response.xpath('//*[@id="fiches_plantes"]/div[2]/div[2]/div[10]/div[3]/div/p[5]').extract()[0].replace('<p>','').replace('<strong>','').replace('</strong>','').replace('\n',' ').replace('</p>',''),
+            #'pest' :  re.sub('<[^>]+>', '', response.xpath('//*[@id="fiches_plantes"]/div[2]/div[2]/div[10]/div[9]/div/p[1]').getall()[0]),
+            #'ecologicalTips' : data_clean('//*[@id="fiches_plantes"]/div[2]/div[2]/div[10]/div[12]/div//p'),
+            #'history' : data_clean('//*[@id="fiches_plantes"]/div[2]/div[2]/div[10]/div[13]/div//p'),
         
         }
