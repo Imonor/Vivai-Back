06a945757b1cadb7fc3ec56589fd40a0040979ef
Imonor <ilan.piperno@gmail.com>
2020-05-06 09:47:06 +0200

    Yessay

--- common/supported_plants.py
+++ common/supported_plants.py
@@ -2,18 +2,14 @@
 
 from botocore.exceptions import ClientError
 
+
 import common.utilities as utilities
 import common.plant_services as plant_services
 import common.db_dealer as db_dealer
-from plant_info.plant_info.spiders.plant_info_spider import PlantInfoSpider
-from scrapy.crawler import CrawlerProcess
-from scrapy.settings import Settings
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from plant_info.plant_info.spiders.plant_info_spider import PlantInfoSpider
 
-import common.utilities as utilities
-import common.db_dealer as db_dealer
+from scrapy import signals
+from scrapy.crawler import Crawler, CrawlerProcess
+from plant_info.plant_info.spiders.plant_info_spider import PlantInfoSpider
 
 PARAM_SPECIES = "species"
 
@@ -21,7 +17,7 @@ def get_plant_infos(species):
     """Checks if species parameter has completed infos in Plant table.
     If not it completes the plant infos by web-scrapping method and returns the plant ID.
     Else it returns the plant ID without web-scrapping"""
-    
+
     try:
         item = db_dealer.get_attributes(db_dealer.PLANT_TABLE, ["id", "picUrl"], "species", "=", species)
 
@@ -42,9 +38,9 @@ def get_plant_infos(species):
             items = items[0]
             picUrl = items.get('picUrl')
             # Ajout de la plante dans la table informative et retour de son ID.
-            
+
             plant_id = plant_services.add_plant(items)
-            
+
             return plant_id, picUrl
 
         # Espèce déjà renseignée dans la table informative
